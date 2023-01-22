b81bc7e3490d4dbfe8330ed2ff8ef39db62f2a93
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2023-01-22 18:25:47 +0530

    chat done

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,36 +1,40 @@
-import { initSockets } from './sockets'
-const body = document.querySelector('body')
-const loginForm = document.getElementById('jsLogin')
+import { initSockets } from "./sockets";
 
-const NICKNAME = 'nickname'
-const LOGGED_OUT = 'loggedOut'
-const LOGGED_IN = 'loggedIn'
+const body = document.querySelector("body");
+const loginForm = document.getElementById("jsLogin");
 
-const nickname = localStorage.getItem(NICKNAME)
+const NICKNAME = "nickname";
+const LOGGED_OUT = "loggedOut";
+const LOGGED_IN = "loggedIn";
+
+const nickname = localStorage.getItem(NICKNAME);
 
 const logIn = (nickname) => {
-  const socket = io('/')
+  const socket = io("/"); // socket연결
+  // socket.emit("setNickname", { nickname })
   socket.emit(window.events.setNickname, { nickname })
-  initSockets(socket)
+  initSockets(socket);
 }
 
-if (nickname === null) {
-  body.className = LOGGED_OUT
+if(nickname === null) {
+  body.className = LOGGED_OUT;
 } else {
-  body.className = LOGGED_IN
-  logIn(nickname)
+  body.className = LOGGED_IN;
+  logIn(nickname);
 }
 
 const handleFormSubmit = (e) => {
-  e.preventDefault()
-  const input = loginForm.querySelector('input')
-  const { value } = input
-  input.value = ''
-  localStorage.setItem(NICKNAME, value)
-  body.className = LOGGED_IN
-  logIn(value)
+  e.preventDefault();
+  const input = loginForm.querySelector("input");
+  // console.log(input.value);
+  const { value } = input;  // input.value
+  //console.log(value);
+  input.value = "";   //input에 작성하고 엔터치면 비워줌
+  localStorage.setItem(NICKNAME, value); // localstorage에 nickname이 저장됨
+  body.className = LOGGED_IN;
+  logIn(value);
 }
 
-if (loginForm) {
-  loginForm.addEventListener('submit', handleFormSubmit)
+if(loginForm){
+  loginForm.addEventListener("submit", handleFormSubmit)
 }
--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,17 +1,17 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
-const fireNotification = (text, color) => {
-  const notification = document.createElement('div')
-  notification.innerText = text
-  notification.style.backgroundColor = color
-  notification.className = 'notification'
-  body.appendChild(notification)
+const fireNotification = (text,color) => {
+  const notification = document.createElement("div");
+  notification.innerText = text;
+  notification.style.backgroundColor = color;
+  notification.className = "notification";
+  body.appendChild(notification);
 }
 
 export const handleNewUser = ({ nickname }) => {
   // console.log(nickname, " just joined");
-  fireNotification(`${nickname} just joined!`, 'rgb(0, 122, 255)')
-}
+  fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)")
+};
 
 export const handleDisconnected = ({ nickname }) =>
-  fireNotification(`${nickname} just left!`, 'rgb(255, 149, 0)')
+ fireNotification(`${nickname} just left!`, "rgb(255, 149, 0)");
